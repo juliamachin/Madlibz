db89f65ce2adaf2803c0e4f9722658c4b8e8a304
juliamachin <juliaamachin@gmail.com>
2023-04-07 17:23:58 -0700

    made axios call to backend

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ import axios from "axios";
 import { Route, Routes, useNavigate } from "react-router-dom";
 
 function App() {
+
+  useEffect(() => {
+    apiCall();
+  }, []);
+
+  const apiCall = async () => {
+    try {
+      let response = await axios.get(
+        `http://localhost:4000/stories`
+      );
+        console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
       <h1>Madlibz</h1>
